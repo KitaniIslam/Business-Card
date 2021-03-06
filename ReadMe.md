dc83c4b20d9ed9bf05e4232e9716a22f8420abb1
kitani islam <slmkitani@gmail.com>
2021-03-06 02:12:01 +0100

    :tada: Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+This NPM pacakge is a virtual business card for me, Kitani Islam.
+
+`npx `
---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+console.log(`🧑 Kitani Islam
+
+👉 Twitter - https://twitter.com/kitani_islam
+👉 Linkedin - https://www.linkedin.com/in/kitani-islam-a0111a14b/
+👉 Github - https://github.com/KitaniIslam
+
+📫 Email - slmkitani@gmail.com`)
